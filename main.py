e55606fc7e2a81aa4a12503c8eeeec742aaf2110
Vasya <htmltak2@gmail.com>
2024-12-01 00:54:38 +0300

    add for cycle and fString

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ succeful_second_answer = "am"
 succeful_first_answer = "is"
 counter = 0
 сounter_answer = 0
-counter_proc = 0
 print("Привет! Предлагаю проверить свои знания английского!Напиши, как тебя зовут?")
 name = input()
 
@@ -19,7 +18,7 @@ def guestion(questi:str,succeful_answer:str):
     answer_1 = input()
 
     if answer_1 == succeful_answer:
-        print("My name is" + name + ", ты + 10 баллов получил!")
+        print( name + " , ты + 10 баллов получил!")
         counter = counter + 10
         сounter_answer = сounter_answer + 1
     else:
@@ -29,7 +28,7 @@ def guestion(questi:str,succeful_answer:str):
 
 
 answers = [succeful_first_answer, succeful_second_answer, succeful_third_answer]
-questions = ["My name __ ", "I ___ a coder.", "I live __ Moscow"]
+questions = [f"My name __ {name}", "I ___ a coder.", "I live __ Moscow"]
 
 for index, velue in enumerate(questions):
     guestion(velue,answers[index])
@@ -50,10 +49,6 @@ if сounter_answer >= 2:
     answerov = ' Вопроса'
 
 сounter_answer = str(сounter_answer)
-procamswer = 30 / 100
-answer = procamswer * float(counter)
-answer = answer * 10
-answer = str(answer)
 counter = str(counter)
 
 if counter_proc > 70:
